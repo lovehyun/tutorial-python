af85bb3155f77417e5a1dc332154bba346759317
Su-Hyun Park <lovehyun@hotmail.com>
2023-06-28 13:48:24 +0900

    [DEV] 마이너 구문 개선

--- 3.flask_basics/2.templates/app1.py
+++ 3.flask_basics/2.templates/app1.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
 @app.route("/<name>")
-def user(name):
+def user(name=""):
     return render_template("index.html", name=name, content=["bill", "steve", "larry"])
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
- 
