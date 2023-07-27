d755bf1d43d2d70f762413c2c32a355f9be5ba00
Su-Hyun Park <lovehyun@hotmail.com>
2023-07-27 15:19:24 +0900

    [DEV] DB 스키마 변경 마이그레션 지원

--- 3.flask_basics/12.login/design/app.py
+++ 3.flask_basics/12.login/design/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -13,6 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
