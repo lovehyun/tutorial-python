5df59e26b9af0c3f6ddd238a9ae56601616f8d78
Su-Hyun Park <lovehyun@hotmail.com>
2023-08-08 14:21:10 +0900

    [DEV] 디자인 개선

--- 3.flask_basics/20.webscrap/8.movie_design/static/scripts2.js
+++ 3.flask_basics/20.webscrap/8.movie_design/static/scripts2.js
@@ -1,3 +1,25 @@
+// Function to change the background to the clicked movie poster
+function changeBackground(posterUrl) {
+    const backgroundOverlay = document.querySelector('.background-overlay');
+    backgroundOverlay.style.backgroundImage = `url(${posterUrl})`;
+    backgroundOverlay.style.opacity = 0.3; // Set the opacity of the background overlay
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const carousel = document.querySelector('.movie-carousel');
+    const cardContainer = carousel.querySelector('.card-container');
+    const prevButton = carousel.querySelector('.carousel-prev');
+    const nextButton = carousel.querySelector('.carousel-next');
+
+    prevButton.addEventListener('click', function () {
+        cardContainer.scrollBy({ left: -cardContainer.clientWidth, behavior: 'smooth' });
+    });
+
+    nextButton.addEventListener('click', function () {
+        cardContainer.scrollBy({ left: cardContainer.clientWidth, behavior: 'smooth' });
+    });
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     const carousel = document.querySelector('.movie-carousel');
     const cardContainer = carousel.querySelector('.card-container');
