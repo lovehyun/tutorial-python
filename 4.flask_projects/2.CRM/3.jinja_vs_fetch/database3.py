8b2222750273d135ffd08313c2ce9d7c69b65891
Su-Hyun Park <lovehyun@hotmail.com>
2024-06-24 15:45:03 +0900

    [DEV] sqlite 의 응답을 row 로 처리

--- 4.flask_projects/2.CRM/3.jinja_vs_fetch/database3.py
+++ 4.flask_projects/2.CRM/3.jinja_vs_fetch/database3.py
@@ -1,9 +1,10 @@
 import sqlite3
 
-DATABASE = 'database/database.db'
+DATABASE = 'database.db'
 
 def get_connection():
     conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row  # 데이터를 행(row) 형태로 반환하도록 설정
     return conn
 
 def get_stores():
@@ -12,6 +13,9 @@ def get_stores():
     cursor.execute("SELECT * FROM stores")
     stores = cursor.fetchall()
     conn.close()
+
+    stores = [dict(row) for row in stores] # Row 타입을 dict 타입으로 변환
+
     return stores
 
 def get_store_by_name(name):
@@ -20,4 +24,7 @@ def get_store_by_name(name):
     cursor.execute("SELECT * FROM stores WHERE Name LIKE ?", ('%' + name + '%',))
     stores = cursor.fetchall()
     conn.close()
+
+    stores = [dict(row) for row in stores] # Row 타입을 dict 타입으로 변환
+
     return stores
