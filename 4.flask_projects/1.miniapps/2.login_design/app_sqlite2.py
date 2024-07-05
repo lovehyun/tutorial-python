e3b0eddb337f25197110decf0993e2ce25cb3b10
Su-Hyun Park <lovehyun@hotmail.com>
2024-07-05 13:35:52 +0900

    [DEV] DB 연결코드 개선

--- 4.flask_projects/1.miniapps/2.login_design/app_sqlite2.py
+++ 4.flask_projects/1.miniapps/2.login_design/app_sqlite2.py
@@ -9,12 +9,42 @@ DATABASE = 'users.db'
 def connect_db():
     return sqlite3.connect(DATABASE)
 
+# 1. 요청시마다 매번 접속/종료
+# def query_db(query, args=(), one=False):
+#     db = connect_db()
+#     cur = db.execute(query, args)
+#     rv = cur.fetchall()
+#     cur.close()
+#     db.close()
+#     return (rv[0] if rv else None) if one else rv
+
+# 2. 연결된 커넥션 지속 사용
+# def get_db():
+#     db = getattr(g, '_database', None)
+#     if db is None:
+#         db = g._database = sqlite3.connect(DATABASE)
+#     return db
+
+# 3. 연결된 커넥션 지속 사용 & 끊어지면 재연결
+def get_db():
+    if 'db' not in g:
+        g.db = connect_db()
+    try:
+        g.db.execute('SELECT 1')  # DB 연결이 유효한지 확인
+    except sqlite3.OperationalError:  # 연결이 유효하지 않으면 재연결 시도
+        g.db = connect_db()
+    return g.db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 def query_db(query, args=(), one=False):
-    db = connect_db()
-    cur = db.execute(query, args)
+    cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
-    db.close()
     return (rv[0] if rv else None) if one else rv
 
 @app.route('/')
