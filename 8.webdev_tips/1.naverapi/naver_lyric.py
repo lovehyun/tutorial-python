ffe49cfa409d2ab89dc777019328090ba88511c2
Su-Hyun Park <lovehyun@hotmail.com>
2024-08-21 20:12:23 +0900

    [DEV] naver 가사검색 엔드포인트 테스트

---
+++ 8.webdev_tips/1.naverapi/naver_lyric.py
@@ -0,0 +1,22 @@
+import requests
+
+def search_music_with_lyrics(query):
+    try:
+        response = requests.get('https://m.search.naver.com/p/csearch/content/qapirender.nhn', params={
+            'where': 'm',
+            'key': 'LyricsSearchResult',
+            'pkid': '519',
+            'u1': 1,
+            'u2': 3,
+            'u3': '0',
+            'u4': '1',
+            'q': '가사검색 ' + query,
+        })
+
+        # 응답 데이터 처리
+        print(response.json())
+    except requests.exceptions.RequestException as error:
+        print('Error fetching lyrics:', error)
+
+# 함수 호출
+search_music_with_lyrics("동해물과 백두산이")
