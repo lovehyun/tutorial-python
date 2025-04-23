0a82a8fabb0b1d9df7f0ae81b7157ea3bdf2b60c
Su-Hyun Park <lovehyun@hotmail.com>
2025-04-24 01:44:17 +0900

    [DEV] 동적 URL 지원하도록 변경

--- 10.trends/6.naverkakao_api/my_kgoogle/app.py
+++ 10.trends/6.naverkakao_api/my_kgoogle/app.py
@@ -48,4 +48,4 @@ def search():
     return render_template('results.html', query=query, results=results, search_type=search_type)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
